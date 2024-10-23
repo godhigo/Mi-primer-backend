bccbcbb7689f2aedd105ddc43f15fd98f439615b
Diego Navarro <godhieguito@gmail.com>
2024-10-23 16:31:36 -0600

    render template

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 #importamos las bibliotecas
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Mi primer intento"
+    return render_template('index.html')
 
 @app.route('/saludos')
 def saludos():
     return "Que onda"
-    
 
 if __name__ == '__main__':
     app.run(debug=True)

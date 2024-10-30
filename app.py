9fc4b44f8b0245848a7bf283dc9aacf8620dc825
Diego Navarro <godhieguito@gmail.com>
2024-10-30 16:54:01 -0600

    Github

--- app.py
+++ app.py
@@ -7,9 +7,41 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/saludos')
-def saludos():
-    return "Que onda"
+@app.route('/presentacion.html')
+def presentacion():
+    return render_template('presentacion.html')
+
+@app.route('/index_t1tarea2.html')
+def x():
+    return render_template('index_t1tarea2.html')
+
+@app.route('/git.html')
+def git():
+    return render_template('git.html')
+
+@app.route('/vr.html')
+def vr():
+    return render_template('vr.html')
+
+@app.route('/scan.html')
+def scan():
+    return render_template('scan.html')
+
+@app.route('/framework.html')
+def framework():
+    return render_template('framework.html')
+
+@app.route('/hangman.html')
+def hangman():
+    return render_template('hangman.html')
+
+@app.route('/entorno_virtual.html')
+def entorno_virtual():
+    return render_template('entorno_virtual.html')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
